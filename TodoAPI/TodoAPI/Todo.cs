885ae368527028d0ba2c06d704bc71f14a8ae3a4
KalleK <s2200829@edu.bc.fi>
2024-10-09 12:18:54 +0300

    tehtava 1

---
+++ TodoAPI/TodoAPI/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoAPI
+{
+    public class Todo
+    {
+        //ei muuttujia vaan ominaisuuskia koska get set metodit
+        public int Id { get; set; }
+        public string? Name { get; set; }  
+
+        public bool IsComplete { get; set; }
+    }
+}
+
+
